dd01728f300fb154dc330039ae9b8987d9db0b9c
Егор Муравьёв <e.muraviev@itpdevelopment.ru>
2023-08-30 18:30:35 +0300

    Добавлен базовый менеджер

---
+++ Client/Managers/BaseManager.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Contracts;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Client.Managers;
+
+public abstract class BaseManager<TEntity> where TEntity : AuditableEntity<Guid>
+{
+    protected HttpClient HttpClient { get; set; }
+    protected virtual string EntityPath { get; set; }
+    public BaseManager(HttpClient httpClient)
+    {
+        HttpClient = httpClient;
+    }
+
+    public async Task<List<TEntity>> GetAllEntitiesAsync()
+    {
+        var response = await HttpClient.GetAsync(EntityPath);
+        var responseAsString = await response.Content.ReadAsStringAsync();
+        var responseEntities = JsonSerializer.Deserialize<List<TEntity>>(responseAsString, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            ReferenceHandler = ReferenceHandler.Preserve
+        });
+
+        return responseEntities.ToList();
+    }
+}
